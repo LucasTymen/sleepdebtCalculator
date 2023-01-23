dd197868b1c5713d2d91284bc8b2a22cb1465ea5
Lucas Tymen <lucas.tymen@gmail.com>
2023-01-23 19:09:18 +0100

    setting_up_days_hours

--- sleepdebtCalculator.js
+++ sleepdebtCalculator.js
@@ -1,7 +1,32 @@
-const getSleepHours = day => {
-  if (day === 'monday'|| day === 'tuesday'|| day === 'wednedsay' || day === 'thursday'|| day === 'wednesday'||day === 'saturday'||day === 'sunday'){
-    return 8;
-  }
-}
+const getSleepHours = (day) => {
+  if (day === "monday") {
+     return 8;
+   } else if (day === "tuesday") {
+     return 8;
+   } else if (day === "wednedsay") {
+     return 8;
+   } else if (day === "thursday") {
+     return 8;
+   } else if (day === "friday") {
+     return 8;
+   } else if (day === "saturday") {
+     return 8;
+   } else if (day === "sunday") {
+     return 8;
+   } else {
+     console.log("wrong spelling");
+     return 8;
+   }
+ };
 
-console.log(getSleepHours('monday'))
+ const getActualSleepHours = () =>
+   getSleepHours("monday") +
+   getSleepHours("tuesday") +
+   getSleepHours("wednedsay") +
+   getSleepHours("thursday") +
+   getSleepHours("friday") +
+   getSleepHours("saturday") +
+   getSleepHours("sunday");
+
+ console.log("----- Summ of sleeping normal hours -----")
+ console.log( getSleepHours("monday") + getSleepHours("tuedsay") + getSleepHours("wednedsay") + getSleepHours("thursday") + getSleepHours("friday") + getSleepHours("saturday") + getSleepHours("sunday"));
